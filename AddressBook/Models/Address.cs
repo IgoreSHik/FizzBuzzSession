473515e7da62386b474fcece43db4c64e7659759
Ihar Balyka <balyko070699@gmail.com>
2021-03-14 13:40:11 +0100

    Добавьте файлы проекта.

---
+++ AddressBook/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [Required(ErrorMessage = "No podaj ulice")]
+        public string Street { get; set; }
+        [StringLength(60, MinimumLength = 6), Required(ErrorMessage = "Podaj conajmnei 6 liczb")]
+        [Display(Name = "ZipCode jak w WinRAR ale pocztowy")]
+        public string ZipCode { get; set; }
+        [Required(ErrorMessage = "No chociaż najblizsze miasto")]
+        [Display(Name = "Ciasto-Miasto")]
+        public string City { get; set; }
+        [Display(Name = "Liczba. Number. Число. Zahl. Πλήθος.")]
+        public int Number { get; set; }
+    }
+}
