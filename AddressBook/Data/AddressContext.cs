6a8dff5fe0c7ff74a144b69f28965f89f1baef20
Ihar Balyka <balyko070699@gmail.com>
2021-05-07 16:44:10 +0200

    1

---
+++ AddressBook/Data/AddressContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Data
+{
+    public class AddressContext : DbContext
+    {
+            public DbSet<Address> Address { get; set; }
+    }
+}
